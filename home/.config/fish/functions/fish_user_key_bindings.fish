518a366d3c0d6a3a7d223696f748f4d926e48a3c
Pev Hall <pev.hall@gmail.com>
2020-07-31 00:06:19 +0930

    .config/nvim/pack/minpac submoulde
    minpac was not added as a sub module correctly. I have fixed its
    location and now it seems to be working well.

--- home/.config/fish/functions/fish_user_key_bindings.fish
+++ home/.config/fish/functions/fish_user_key_bindings.fish
@@ -7,6 +7,7 @@ function fish_user_key_bindings
 #    if test "$__fish_active_key_bindings" = "fish_vi_key_bindings"
 #        bind -M insert \c] "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 #    end
+
+	fzf_key_bindings
 end
 
-fzf_key_bindings
